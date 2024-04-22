007d6cadf92a766b31975daf2c2ba43b69277fe6
Esoftdev0 <125973085+Esoftdev0@users.noreply.github.com>
2024-04-22 13:17:47 +0100

    haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,26 +1,19 @@
-const adjs = [
-  'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
-  'summer', 'icy', 'delicate', 'quiet', 'white', 'cool', 'spring', 'winter',
-  'patient', 'twilight', 'dawn', 'crimson', 'wispy', 'weathered', 'blue',
-  'billowing', 'broken', 'cold', 'damp', 'falling', 'frosty', 'green',
-  'long', 'late', 'lingering', 'bold', 'little', 'morning', 'muddy', 'old',
-  'red', 'rough', 'still', 'small', 'sparkling', 'throbbing', 'shy',
-  'wandering', 'withered', 'wild', 'black', 'young', 'holy', 'solitary',
-  'fragrant', 'aged', 'snowy', 'proud', 'floral', 'restless', 'divine',
-  'polished', 'ancient', 'purple', 'lively', 'nameless'
-];
+const adjs = [  'ab1', 'ab2', 'ab3', 'ab4', 'ab5', 'ab6', 'ab7', 'ab8', 'ab9', 'ab10',  
+'ab11', 'ab12', 'ab13', 'ab14', 'ab15', 'ab16', 'ab17', 'ab18', 'ab19', 'ab20',  'ab21', 
+'ab22', 'ab23', 'ab24', 'ab25', 'ab26', 'ab27', 'ab28', 'ab29', 'ab30',  'ab31', 'ab32',
+ 'ab33', 'ab34', 'ab35', 'ab36', 'ab37', 'ab38', 'ab39', 'ab40',  'ab41', 'ab42', 'ab43',
+ 'ab44', 'ab45', 'ab46', 'ab47', 'ab48', 'ab49', 'ab50',  'ab51', 'ab52', 'ab53', 'ab54',
+ 'ab55', 'ab56', 'ab57', 'ab58', 'ab59', 'ab60',  'ab61', 'ab62', 'ab63', 'ab64'];
 
-const nouns = [
-  'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea', 'morning',
-  'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf', 'dawn', 'glitter',
-  'forest', 'hill', 'cloud', 'meadow', 'sun', 'glade', 'bird', 'brook',
-  'butterfly', 'bush', 'dew', 'dust', 'field', 'fire', 'flower', 'firefly',
-  'feather', 'grass', 'haze', 'mountain', 'night', 'pond', 'darkness',
-  'snowflake', 'silence', 'sound', 'sky', 'shape', 'surf', 'thunder',
-  'violet', 'water', 'wildflower', 'wave', 'water', 'resonance', 'sun',
-  'wood', 'dream', 'cherry', 'tree', 'fog', 'frost', 'voice', 'paper',
-  'frog', 'smoke', 'star'
-];
+
+const nouns = [  'esoft1', 'esoft2', 'esoft3', 'esoft4', 'esoft5', 'esoft6', 'esoft7', 'esoft8',
+  'esoft9', 'esoft10', 'esoft11', 'esoft12', 'esoft13', 'esoft14', 'esoft15', 'esoft16',  'esoft17',
+  'esoft18', 'esoft19', 'esoft20', 'esoft21', 'esoft22', 'esoft23', 'esoft24',  'esoft25', 'esoft26', 
+  'esoft27', 'esoft28', 'esoft29', 'esoft30', 'esoft31', 'esoft32',  'esoft33', 'esoft34', 'esoft35',
+  'esoft36', 'esoft37', 'esoft38', 'esoft39', 'esoft40',  'esoft41', 'esoft42', 'esoft43', 'esoft44',
+  'esoft45', 'esoft46', 'esoft47', 'esoft48',  'esoft49', 'esoft50', 'esoft51', 'esoft52', 'esoft53',
+  'esoft54', 'esoft55', 'esoft56',  'esoft57', 'esoft58', 'esoft59', 'esoft60', 'esoft61', 'esoft62', 
+  'esoft63', 'esoft64'];
 
 module.exports = () => {
   const adj = adjs[Math.floor(Math.random() * adjs.length)];
